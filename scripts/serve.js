36171563a9bd137326392cfd34bfb19c7d5ae256
Hans Fast <hans@surfacemarkup.net>
2018-04-10 14:44:48 +0200

    finish test setup and add docker-compose

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,38 +1,12 @@
 const http = require('http')
 const connect = require('connect')
 const serveStatic = require('serve-static')
-const puppeteer = require('puppeteer');
-const tap = require('tap');
-const pa11y = require('pa11y');
+const test = require('../test/test.js');
 
 
-// Simple server for serving static files
+// Simple server for serving static files with test in callback
 const app = connect().use(serveStatic('test/'))
 const server = http.createServer(app).listen(8123, () => {
   test(server);
 })
 
-async function test(server){
-  await tap.test('there is a map div present', async function(t) {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:8123/test.html');
-    const div = await page.$eval('#mapdiv', el => el ? true : false);
-    t.assert(div === true, 'div is present');
-    const map = await page.evaluate(() => map.getCenter());
-    t.assert(map.lng === 5.9699 && map.lat=== 52.2112, 'map is centered as expected')
-    await browser.close();
-    t.end()
-  })
-  await tap.test('there are no accessibility issues', async function(t) {
-    const ariares = await pa11y('http://localhost:8123/test.html');
-    t.assert(ariares.issues.length === 0, 'issue length is 0')
-    t.end()
-    
-  })
-
-  server.close();
-
-
-
-}
--- test/test.js
+++ test/test.js
@@ -1,1 +1,38 @@
-let puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
+const tap = require('tap');
+const pa11y = require('pa11y');
+
+async function test(server){
+  //General browser tests
+  await tap.test('The map is loaded', async function(t) {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8123/test.html');
+    const div = await page.$eval('#mapdiv', el => el ? true : false);
+    t.assert(div === true, 'div is present');
+    const mapCenter = await page.evaluate(() => map.getCenter());
+    t.assert(mapCenter.lng === 5.9699 && mapCenter.lat=== 52.2112, 'map is centered as expected')
+    await browser.close();
+    t.end()
+  })
+  //ARIA tests
+  await tap.test('there are no accessibility issues', async function(t) {
+    const ariares = await pa11y('http://localhost:8123/test.html', {
+      allowedStandards: 'WCAG2AA',
+      level: 'error',
+      chromeLaunchConfig: {
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+      }
+    });
+    t.assert(ariares.issues.length === 0, 'length ofissue list is 0')
+    t.end()
+    
+  })
+
+  server.close();
+}
+
+module.exports = test;
