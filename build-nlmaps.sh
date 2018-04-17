f835528b0de70469d0e65f5e72afcb7f71f4c37b
Hans Fast <hans@surfacemarkup.net>
2018-04-17 12:22:01 +0200

    relocate config file

--- build-nlmaps.sh
+++ build-nlmaps.sh
@@ -2,5 +2,5 @@
 git clone https://github.com/webmapper/nlmaps
 cd nlmaps
 npm install
-NODE_ENV=production node scripts/build -c ../amsterdam.config.js
+NODE_ENV=production node scripts/build -c ../config/amsterdam.config.js
 node scripts/publish
