7a642213d8ebf125b9fd010ee86a94e93a10cfde
Hans Fast <hans@surfacemarkup.net>
2018-04-17 10:59:20 +0200

    test config and fix subsequent async problems
    added a test, then there were several issues that came up with the whole test script hanging because servers weren't being closed due to unresolved promises.
    
    This is fixed now, but the only thing that isn't covered is undefined variables in test assertions.

--- test/test.js
+++ test/test.js
@@ -1,24 +1,39 @@
 const puppeteer = require('puppeteer');
-const tap = require('tap');
+const test = require('tap').test;
 const pa11y = require('pa11y');
 
-async function test(server) {
+
+async function runTests(server) {
   // General browser tests
-  await tap.test('The map is loaded', async (t) => {
+  await test('The map is loaded', async (t) => {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto('http://localhost:8123/index.html');
     const div = await page.$eval('#mapdiv', el => !!el);
-    t.assert(div === true, 'div is present');
+    await t.assert(div === true, 'div is present');
     /* map call occurs in browser context */
     /* eslint-disable-next-line no-undef */
     const mapCenter = await page.evaluate(() => map.getCenter());
-    t.assert(mapCenter.lng === 5.9699 && mapCenter.lat === 52.2112, 'map is centered as expected');
+    await t.assert(mapCenter.lng === 5.9699 && mapCenter.lat === 52.2112, 'map is centered as expected');
     await browser.close();
     t.end();
   });
+  await test('correct config is loaded', async (t) => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8123/index.html');
+    /* eslint-disable no-undef */
+    const url_for_standard = await page.evaluate(() => {
+      const layer = nlmaps.leaflet.bgLayer('standaard');
+      return layer._url;
+    });
+    /* eslint-enable no-undef */
+    await t.assert(url_for_standard === 'https://geodata.nationaalgeoregister.nl/tiles/service/wmts/brtachtergrondkaart/EPSG:3857/{z}/{x}/{y}.png', 'url for created layer is as expected');
+    await browser.close();
+    t.end();
+  })
   // ARIA tests
-  await tap.test('there are no accessibility issues', async (t) => {
+  await test('there are no accessibility issues', async (t) => {
     const ariares = await pa11y('http://localhost:8123/index.html', {
       allowedStandards: 'WCAG2AA',
       level: 'error',
@@ -29,11 +44,10 @@ async function test(server) {
         ],
       },
     });
-    t.assert(ariares.issues.length === 0, 'length ofissue list is 0');
+    await t.assert(ariares.issues.length === 0, 'length ofissue list is 0');
     t.end();
   });
-
   server.close();
 }
 
-module.exports = test;
+module.exports = runTests;
