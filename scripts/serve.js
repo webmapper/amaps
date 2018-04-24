62859cff08ece8a1e893230181103b61c0e1f7bd
Boris Arkenaar <boris@arkenaar.com>
2018-04-24 11:57:30 +0200

    update docker compose configuration

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,9 +1,1 @@
-const http = require('http');
-const connect = require('connect');
-const serveStatic = require('serve-static');
-const test = require('../test/test.js');
-// Simple server for serving static files with test in callback
-const app = connect().use(serveStatic('test/'));
-const server = http.createServer(app).listen(8123, () => {
-  test(server);
-});
+require('../test/test.js')();
