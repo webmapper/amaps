06604b3f32aa3b0cbe8a47fcd62870bae8684770
Boris Arkenaar <boris@arkenaar.com>
2018-04-30 09:43:17 +0200

    move link creation from build-nlmaps.sh to package.json

--- build-nlmaps.sh
+++ build-nlmaps.sh
@@ -7,4 +7,3 @@ npm run bootstrap
 NODE_ENV=production node scripts/build -c ../config/amsterdam.config.js -p leaflet,openlayers,googlemaps,geolocator
 NODE_ENV=production node scripts/build -c ../config/amsterdam.config.js -p nlmaps
 node scripts/publish
-ln -s ../nlmaps test/nlmaps
