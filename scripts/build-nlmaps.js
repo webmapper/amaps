c2d875133212deb213ae702a1f6d6859aad55e17
Hans Fast <hans@webmapper.net>
2018-06-11 14:22:46 +0200

    another linting error fixed

--- scripts/build-nlmaps.js
+++ scripts/build-nlmaps.js
@@ -1,7 +1,9 @@
 const { execSync } = require('child_process');
 
 process.env.NODE_ENV = 'production';
+/* eslint-disable-next-line max-len */
 execSync('node scripts/build.js -p leaflet,googlemaps,openlayers,geolocator -c ../config/amsterdam.config.js', {cwd: '/app/nlmaps', env: process.env, stdio: [0,1,2]});
+/* eslint-disable-next-line max-len */
 execSync('node scripts/build.js -p nlmaps -c ../config/amsterdam.config.js', {cwd: 'nlmaps', env: process.env, stdio: [0,1,2]})
 execSync('node scripts/publish', {cwd: 'nlmaps', env: process.env, stdio: [0,1,2]})
 
