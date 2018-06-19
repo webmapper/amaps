3c04d5376a1e7e617ba1d0fc7bbf09ce9ccb5a23
Hans Fast <hans@surfacemarkup.net>
2018-06-19 17:07:28 +0200

    some buid tweaks and nlmaps version update
    Squashed commit of the following:
    
    commit d94a6c25084331a93de9f25ee9619a504c13e9fd
    Author: Hans Fast <hans@surfacemarkup.net>
    Date:   Thu Jun 14 12:03:22 2018 +0200
    
        exclude nlmaps from babelification
    
    commit f2e76d1c53693758a32c0261c9bcb7c5d72cffba
    Author: Hans Fast <hans@surfacemarkup.net>
    Date:   Thu Jun 14 11:56:04 2018 +0200
    
        nlmaps version
    
    commit 9dbd6306bb7a62509b6beea3bac291f8084e94ba
    Author: Hans Fast <hans@surfacemarkup.net>
    Date:   Thu Jun 14 11:45:12 2018 +0200
    
        remove absolute paths in npm scripts for nlmaps
    
    commit be3ba2da914fb501ab6a3c43a7321074c31125c3
    Author: Hans Fast <hans@surfacemarkup.net>
    Date:   Thu Jun 14 11:44:35 2018 +0200
    
        installation

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,8 @@ export default {
   plugins: [
     json(),
     babel({
-      exclude: 'node_modules/**',
+      exclude: ['**/node_modules/**', '**/nlmaps/**'],
+      babelrc: false,
       presets: [[
         'env',
         {
