d4a70d0f807fa3db29dca98dd26cb760f23fce07
Steven Ottens <github@minst.net>
2018-07-05 11:04:08 +0200

    babel-polyfill added to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 /*amaps is just nlmaps built with Amsterdam's config. So let's rename it amaps.*/
 import { nlmaps } from '../nlmaps/packages/nlmaps/src/index.js';
 export default nlmaps;
